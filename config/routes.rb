992b3dc4992a6a85244461d2a898c52bdc5ce112
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-04-21 05:47:49 -0300

    Restart deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root "application#hello"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
