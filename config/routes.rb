ec197039cf8d7ba56cf5bff012c26e80e06c900f
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-05-30 01:41:46 -0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   resources :microposts
   resources :users
-  root 'users#index'
+  root "static_pages#home"
 end
