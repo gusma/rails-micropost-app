4c3118e5d3fb9531c84e0c493226c6e9da381356
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-06-06 01:10:19 -0300

    Filling in layout (#7)
    * Add extra options
    
    * Adds to layouting, adds tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-  respond_to do |format|
+    respond_to do |format|
       if @user.save
         format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
   resources :microposts
   resources :users
-  root "static_pages#home"
+  root 'static_pages#home'
 end
