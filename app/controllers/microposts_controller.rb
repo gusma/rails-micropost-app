76d0feb043a9f136c4f569d7495264f03dcfb483
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-08-14 03:44:29 -0300

    Update rubocop rules

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MicropostsController < ApplicationController
   before_action :set_micropost, only: %i[show edit update destroy]
 
@@ -23,7 +25,7 @@ class MicropostsController < ApplicationController
 
     respond_to do |format|
       if @micropost.save
-        format.html { redirect_to micropost_url(@micropost), notice: 'Micropost was successfully created.' }
+        format.html { redirect_to micropost_url(@micropost), notice: "Micropost was successfully created." }
         format.json { render :show, status: :created, location: @micropost }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -36,7 +38,7 @@ class MicropostsController < ApplicationController
   def update
     respond_to do |format|
       if @micropost.update(micropost_params)
-        format.html { redirect_to micropost_url(@micropost), notice: 'Micropost was successfully updated.' }
+        format.html { redirect_to micropost_url(@micropost), notice: "Micropost was successfully updated." }
         format.json { render :show, status: :ok, location: @micropost }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -50,7 +52,7 @@ class MicropostsController < ApplicationController
     @micropost.destroy
 
     respond_to do |format|
-      format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
+      format.html { redirect_to microposts_url, notice: "Micropost was successfully destroyed." }
       format.json { head :no_content }
     end
   end
--- test/helpers/application_test_helper.rb
+++ test/helpers/application_test_helper.rb
@@ -1,8 +1,10 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
-  test 'full title helper' do
+  test "full title helper" do
     assert_equal FILL_IN, full_title
-    assert_equal FILL_IN, full_title('Help')
+    assert_equal FILL_IN, full_title("Help")
   end
 end
