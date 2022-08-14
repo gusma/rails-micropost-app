2e5cd27a34b9ee16d9cf58214a8f7fdfd130cc12
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-08-14 03:44:29 -0300

    Update rubocop rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get '/signup', to: 'users#new'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get "sessions/new"
+  get "/signup", to: "users#new"
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   resources :microposts
   resources :users
-  root 'static_pages#home'
+  root "static_pages#home"
 end
