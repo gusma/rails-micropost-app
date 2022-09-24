e44dca1c6cc47697ea2d60f71fbc4d7e53ff8271
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-09-24 02:54:17 -0300

    Update tests and helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+    if @user&.authenticate(params[:session][:password])
       reset_session
       params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
       log_in @user
